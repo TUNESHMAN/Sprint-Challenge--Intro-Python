b8a8a7a2f3b868842ad23b3d0a1ab96a9baeb339
Babatunde <babatundea15@gmail.com>
2020-06-05 08:41:27 +0100

    i created the parent class vehicle and the child

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,26 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# The vehicle class is the parent class
+class Vehicle:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return f"The vehicle is {self.name}"
+
+
+class GroundVehicle(Vehicle):
+    def __init__(self, name, brand):
+        super().__init__(name)
+        self.brand = brand
+
+    def __str__(self):
+        return super().__str__() + f" and it's brand is {self.brand}"
+
+
+vehicle = Vehicle("speed car")
+groundVehicle = GroundVehicle("speed car", "Toyota")
+print(vehicle)
+print(groundVehicle)
